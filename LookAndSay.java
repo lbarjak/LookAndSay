e7de8d4684d3878e12a138f976256502eb31b377
Laszlo Barjak <barjak.laszlo@gmail.com>
2018-10-10 01:11:21 +0200

    Update LookAndSay.java

--- LookAndSay.java
+++ LookAndSay.java
@@ -4,7 +4,7 @@ public void lookAndSay() {
     for (int j = 0; j < 10; j++) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < num.length(); i++) {
-            if (i < num.length() - 1 && num.charAt(i) == (num.charAt(i + 1))) {
+            if (i < num.length() - 1 && num.charAt(i) == num.charAt(i + 1)) {
                 counter++;
             } else {
                 result.append(counter).append(num.charAt(i));
